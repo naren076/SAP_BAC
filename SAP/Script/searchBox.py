e6baa199a0241bead77e845d1ccc4fe92e54beba
Vaishnavi Ramachandraprabakaran <vaishnavi.r@mksvision.com>
2023-04-21 16:52:31 +0530

    waitnamedchild_fix

--- SAP/Script/searchBox.py
+++ SAP/Script/searchBox.py
@@ -1,6 +1,8 @@
 ﻿def search_item(page, search_value):
-  page.FindElement("//a[@id='sf']/span").Click()
-  page.WaitConfirm(5000)
-  searchBox = page.FindElement("//div[@id='searchFieldInShell-input-content']/input")
-  searchBox.SetText(search_value)
-  searchBox.Keys("[Enter]")
+  if(NameMapping.Sys.browser.pageFlp.WaitNamedChild("textnode", 10000).Exists):
+    page.FindElement("//a[@id='sf']/span").Click()
+  page.WaitConfirm(3000)
+  if(NameMapping.Sys.browser.pageFlp.WaitNamedChild("searchboxSearchfieldinshellInput", 10000).Exists):
+    searchBox = page.FindElement("//div[@id='searchFieldInShell-input-content']/input")
+    searchBox.SetText(search_value)
+    searchBox.Keys("[Enter]")
